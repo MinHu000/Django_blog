ffad53f28607e51ca5536400bb72734803dd2692
MinHu000 <tpoc822@gmail.com>
2025-12-30 11:32:40 +0900

    Add files via upload

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,21 @@
 
 
-# Register your models here.
+# blog/admin.py
 from django.contrib import admin
 from .models import Post
+from django.conf import settings
 
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "category", "created_at")
+
+    class Media:
+        css = {
+            "all": ("admin/minhu_admin.css",)
+        }
+
+
+admin.site.site_header = settings.ADMIN_SITE_HEADER
+admin.site.site_title = settings.ADMIN_SITE_TITLE
+admin.site.index_title = settings.ADMIN_INDEX_TITLE
